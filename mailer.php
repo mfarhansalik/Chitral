e89e860655e317b6bf5f0f001994dea837874dc9
mfarhansalik <66422058+mfarhansalik@users.noreply.github.com>
2020-12-03 07:04:54 -0500

    Final Website
    Final Website Project

---
+++ mailer.php
@@ -0,0 +1,23 @@
+<?php
+//I will be working on this website in the future so i thought i add this.
+
+// $errors = [];
+
+// if (!empty($_POST)) {
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+// $tel = $_POST['tel'];
+// $message = $_POST['message'];
+//     $toEmail = 'example@example.com';
+//     $emailSubject = 'New email from your contant form';
+//     $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
+
+//     $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Telephone: {$tel}", "Message:", $message];
+//     $body = join(PHP_EOL, $bodyParagraphs);
+
+//     if (mail($toEmail, $emailSubject, $body, $headers)) {
+//         header('Location: index.html');
+//     } else {
+//         $errorMessage = 'Oops, something went wrong. Please try again later';
+//     }
+// }
